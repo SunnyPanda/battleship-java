425213371b26072f8ad253ac9fb0c6083135c67b
Ekaterina Kozlova <ekaterina.kozlova@sbb.ch>
2021-04-12 20:10:53 +0300

    Stage 1: add converter class

---
+++ Battleship/task/src/battleship/Converter.java
@@ -0,0 +1,32 @@
+package battleship;
+
+public class Converter {
+
+    public int[] convertCoordinates(String[] coordinates) {
+        int[] convertedCoordinates = new int[4];
+        int[] start = convertCoordinate(coordinates[0]);
+        int[] end = convertCoordinate(coordinates[1]);
+        convertedCoordinates[0] = Math.min(start[0], end[0]);
+        convertedCoordinates[1] = Math.min(start[1], end[1]);
+        convertedCoordinates[2] = Math.max(start[0], end[0]);
+        convertedCoordinates[3] = Math.max(start[1], end[1]);
+
+        return convertedCoordinates;
+    }
+
+    private int[] convertCoordinate(String coordinate) {
+        int[] cell = new int[2];
+        cell[0] = convertRow(coordinate.substring(0, 1));
+        cell[1] = convertColumn(coordinate.substring(1));
+        return cell;
+    }
+
+    private int convertRow(String row) {
+        String rows = "ABCDEFGHIJ";
+        return rows.indexOf(row);
+    }
+
+    private int convertColumn(String col) {
+        return Integer.parseInt(col);
+    }
+}
--- Battleship/task/src/battleship/Field.java
+++ Battleship/task/src/battleship/Field.java
@@ -2,6 +2,7 @@ package battleship;
 
 public class Field {
     private final char[][] field;
+    private final Converter converter = new Converter();
 
     Field() {
         field = new char[10][11];
@@ -27,24 +28,8 @@ public class Field {
     }
 
     public int[] convertCoordinates(String[] coordinates) {
-        int[] convertedCoordinates = new int[4];
-        int[] start = convertCoordinate(coordinates[0]);
-        int[] end = convertCoordinate(coordinates[1]);
-        convertedCoordinates[0] = Math.min(start[0], end[0]);
-        convertedCoordinates[1] = Math.min(start[1], end[1]);
-        convertedCoordinates[2] = Math.max(start[0], end[0]);
-        convertedCoordinates[3] = Math.max(start[1], end[1]);
-
-        return convertedCoordinates;
+        return converter.convertCoordinates(coordinates);
     }
-
-    private int[] convertCoordinate(String coordinate) {
-        int[] cell = new int[2];
-        cell[0] = convertRow(coordinate.substring(0, 1));
-        cell[1] = convertColumn(coordinate.substring(1));
-        return cell;
-    }
-
 
     public void placeShip(int[] coordinates) {
         int startRow = coordinates[0];
@@ -64,15 +49,6 @@ public class Field {
                 field[row][startCol] = 'O';
             }
         }
-    }
-
-    private int convertRow(String row) {
-        String rows = "ABCDEFGHIJ";
-        return rows.indexOf(row);
-    }
-
-    private int convertColumn(String col) {
-        return Integer.parseInt(col);
     }
 
     public void validateCoordinates(int[] coordinates, Ships ship) throws Exception {
